c9700e67f24822907096e598ce733d2a9d4b053c
Mete Balcı <metebalci@gmail.com>
2023-02-03 08:54:09 +0100

    Create zfs-dirty.d

---
+++ zfs-dirty.d
@@ -0,0 +1,26 @@
+#!/usr/sbin/dtrace -s
+
+# displays the amount of data synced from dirty data memory to the pool and how much it took to sync it (write it to the pool)
+# example output
+# CPU     ID                    FUNCTION:NAME
+#   2  82272                  none:txg-synced 7464MB of 9804MB synced in 32.64 seconds
+
+txg-syncing
+/((dsl_pool_t *)arg0)->dp_spa->spa_name == $$1/
+{
+        start = timestamp;
+        this->dp = (dsl_pool_t *) arg0;
+        d_total = this->dp->dp_dirty_total;
+        d_max = `zfs_dirty_data_max`;
+}
+
+txg-synced
+/start && ((dsl_pool_t *)arg0)->dp_spa->spa_name == $$1/
+{
+        this->d = timestamp - start;
+        printf("%4dMB of %4dMB synced in %d.%02d seconds",
+                d_total / 1024 / 1024,
+                d_max / 1024 / 1024,
+                this->d / 1000000000,
+                (this->d / 1000000) % 100);
+}
